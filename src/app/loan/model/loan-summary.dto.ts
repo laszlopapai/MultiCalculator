847542a5a1277ea61eaf1b86ff23142ed123a036
Laszlo Papai <13553984+laszlopapai@users.noreply.github.com>
2021-11-23 14:34:13 +0100

    Auto-generated commit [ci skip]

--- src/app/loan/model/loan-summary.dto.ts
+++
@@ -1,9 +0,0 @@
-export interface LoanSummary {
-    name: string;
-    duration: number;
-    fee: number;
-    interest: number;
-    prePayment: number;
-    payed: number;
-    realAPR: number; // percentage
-}
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  numberPrecision: '1.2-4',
-  dateFormat: 'yyyy MMM'
-};
